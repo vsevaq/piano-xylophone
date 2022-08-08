58993358fa20e1bbf0e0ed4961a04b51acbadff2
vsevaq <vsevolodq@gmail.com>
2022-08-09 00:30:19 +0300

    Complete Xylophone project

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">Xylophone APP</h1>
+
+<center><img alt="A screen record of iphone " src="images/ScreenRecord.gif" height="550" /></center>
---
+++ android/app/src/main/java/com/example/pianoxylophone/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.pianoxylophone;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
